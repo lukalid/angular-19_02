3efa8e106f8fae8b8e1b282bf723d801f3b3a6d7
lmiletic <l.miletic@msgnetconomy.net>
2019-06-14 09:45:35 +0200

    Navigation Solution 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  recipesClicked = false;
+  shoppingListClicked = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRecipesClicked(recipesClicked) {
+    this.recipesClicked = recipesClicked;
+  }
+
+  onShoppingListClicked(shoppingListClicked) {
+    this.shoppingListClicked = shoppingListClicked;
+  }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+
+  @Output() recipesClicked = new EventEmitter<boolean>();
+  @Output() shoppingListClicked = new EventEmitter<boolean>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClickRecipes() {
+    this.recipesClicked.emit(true);
+    this.shoppingListClicked.emit(false);
+  }
+
+  onClickShoppingList() {
+    this.shoppingListClicked.emit(true);
+    this.recipesClicked.emit(false);
+  }
+
+  ngAfterViewInit(): void {
+    this.recipesClicked.emit(false);
+    this.shoppingListClicked.emit(false);
   }
 
 }
