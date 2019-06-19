37c24ffa3a1c9997b914d90a17e7ea2055156d66
lmiletic <l.miletic@msgnetconomy.net>
2019-06-19 15:44:00 +0200

    Moved providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
-import {RecipeService} from './recipes/recipe.service';
 import {HttpClientModule} from '@angular/common/http';
-import {PersistenceService} from './shared/persistence.service';
-import {AuthService} from './auth/auth.service';
-import {AuthGuard} from './auth/auth-guard.service';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
@@ -24,13 +19,6 @@ import {CoreModule} from './core/core.module';
     SharedModule,
     ShoppingListModule,
     AuthModule
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    PersistenceService,
-    AuthService,
-    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,10 @@ import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
 import {SharedModule} from '../shared/shared.module';
 import {AppRoutingModule} from '../app-routing.module';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {RecipeService} from '../recipes/recipe.service';
+import {PersistenceService} from '../shared/persistence.service';
+import {AuthService} from '../auth/auth.service';
 
 @NgModule({
   declarations: [
@@ -16,6 +20,12 @@ import {AppRoutingModule} from '../app-routing.module';
   exports: [
     AppRoutingModule,
     HeaderComponent
-  ]
+  ],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    PersistenceService,
+    AuthService
+  ],
 })
 export class CoreModule { }
