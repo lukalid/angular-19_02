e331a2028894ad24389a7599ee57031b19c0cfee
lmiletic <l.miletic@msgnetconomy.net>
2019-06-19 13:19:03 +0200

    Sending token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,25 @@ import * as firebase from 'firebase';
 
 @Injectable()
 export class AuthService {
+
+  token: string;
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch((error) => console.log(error));
   }
+
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((response) => console.log(response))
+      .then((response) => firebase.auth().currentUser.getIdToken().then(
+        (token) => this.token = token
+      ))
       .catch((error) => console.log(error));
   }
+
+  getToken() {
+    firebase.auth().currentUser.getIdToken().then((token) => this.token = token);
+    return this.token;
+  }
+
 }
--- src/app/shared/persistence.service.ts
+++ src/app/shared/persistence.service.ts
@@ -2,20 +2,21 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RecipeModel} from '../recipes/recipe.model';
 import 'rxjs/Rx';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class PersistenceService {
 
   url = 'https://angular-recipes-luka.firebaseio.com/recipes.json';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   saveRecipes(recipes: RecipeModel[]) {
-    return this.http.put(this.url, recipes);
+    return this.http.put(this.getUrlWithToken(), recipes);
   }
 
   getRecipes() {
-    return this.http.get<RecipeModel[]>(this.url)
+    return this.http.get<RecipeModel[]>(this.getUrlWithToken())
       .map(recipes => {
         for (const recipe of recipes) {
           if (!recipe['ingredients']) {
@@ -24,6 +25,10 @@ export class PersistenceService {
         }
         return recipes;
       });
+  }
+
+  getUrlWithToken() {
+    return this.url + '?auth=' + this.authService.getToken();
   }
 
 }
